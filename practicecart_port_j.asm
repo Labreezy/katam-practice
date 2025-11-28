3e71fa3f493f373c618f84f4e776067bbef3297b
Ethan R. <yourtooslow@gmail.com>
2025-11-28 09:36:49 -0600

    Update practicecart_port_j.asm to actually function

--- practicecart_port_j.asm
+++ practicecart_port_j.asm
@@ -1,20 +1,22 @@
 .gba
 
-.open "katam_jp.gba","katam_jp_practice.gba",0x08000000 
+.open "KatAM_J.gba","KatAM_J_practice.gba",0x08000000 
 
-//This address provides some free space we can use in this game (i hope).
+
 
 
 //Defines
 
 EntityDataP1 EQU 0x2020EE0
-AREA_START EQU 0x0815BDB0
+AREA_START EQU 0x08D86AF0
 BTN_RIGHT EQU 0x10
 BTN_LEFT EQU 0x20
 BTN_SELECT EQU 0x4
 BTN_A EQU 0x1
 
-
+.org 0x08F9FFE0
+	.ascii "KatAM Practice Cart"
+	.db 0xf, 0x0
 
 //Patches
 
@@ -32,12 +34,21 @@ BTN_A EQU 0x1
 .org 0x0804F9A2
 	nop 
 	nop
+.org 0x08007F6A
+	nop
+	nop
 
-.org 0x0804FFAA
+
+.org 0x0804FFAA //don't lose lives
 	sub r0, r1, #0
 
 
 //Custom Code
+
+//Function pointer to hook
+.org 0x08056D8C
+initChooseAbilityHook:
+.dw org(InitChooseAbility)+1
 
 
 .org AREA_START
@@ -54,22 +65,23 @@ InitChooseAbility:
 	ldr r7, =EntityDataP1+0xDD //next ability
 	strb r4, [r7,0x0]
 
-	ldr r7, =EntityDataP1+0x78 //code pointer
+	ldr r7, =EntityDataP1
 	ldr r4, =org(ChooseAbility)+1
+	str r4, [r7,0x78] //code pointer
 	pop {r4-r7}
 	pop r0
 	bx r0
 
 
 ChooseAbility:
-	push {r4-r7}
+	push {r4-r7,lr}
 
 	//Show Ability Icon
 	ldr r7, =0x03003AD0
 	mov r4, #0x06
 	strb r4, [r7,0x0]
-	ldr r7, =0x3003AD0-0x10
-	mov r7, 0x0C
+	ldr r7, =0x03003AC0
+	mov r4, 0x0C
 	strb r4, [r7,0x0]
 
 	//Restore ability (magic)
@@ -78,51 +90,60 @@ ChooseAbility:
 	strb r4, [r7,0x0]
 
 	ldr r7,=EntityDataP1+0x11A // buttons
-
-	cmp r7,BTN_LEFT
-	beq pressLeft
-	cmp r7, BTN_RIGHT
-	beq pressRight
-	cmp r7, BTN_SELECT
-	beq pressSelect
-	cmp r7, BTN_A
-	beq pressA
+	ldrb r4,[r7,0x0]
+	cmp r4, 0x10
+	beq pressright
+	cmp r4, 0x20
+	beq pressleft
+	cmp r4, 0x04
+	beq pressselect
+	cmp r4, 0x01
+	beq preparereturn
 	b done
 
-	pressLeft:
+	pressleft:
 		ldr r7,=EntityDataP1+0xDD //queued ability
 		ldrb r4, [r7,0x0]
 
-		cmp r4,0
+		cmp r4, 0x0
 		bne no_wrap_left
-			mov r4, #0x1A
-		no_wrap_left:
-			sub r4, r4, #1
-		strb r4,[r7,0x0]
+		mov r4,0x1A
+		strb r4, [r7,0x0]
 		b updateGraphics
-
-	pressRight: 
-		ldr r7,=EntityDataP1+0xDD //queued ability
-		ldrb r4, [r7,0x0]
-		add r4, r4, #1
-		cmp r4, #0x1B
-		bcc no_wrap_right
-		mov r4,#0
-	no_wrap_right:
-		mov r4,0x0
+	no_wrap_left:
+		sub r4, r4, #1
 		strb r4,[r7,0]
 		b updateGraphics
 
-	pressSelect:
+	pressright:
+		ldr r7,=EntityDataP1+0xDD //queued ability
+		ldrb r4, [r7,0x0]
+		add r4, r4, #1
+		cmp r4, 0x1B
+		bcc no_wrap_right
+		mov r4,0x0
+	no_wrap_right:
+		strb r4,[r7,0]
+		b updateGraphics
+
+	pressselect: 
 		mov r4, #0
 		ldr r7, =EntityDataP1+0x100
 		strb r4,[r7,0]
 		ldr r7, =0x03000514
-		strb r4,[r7,0]
-	pressA:
+		strb r4,[r7,0] 
+		//b preparereturn
+
+
+	prepareReturn:
 		ldr r7, =0x02020F58
 		ldr r4, =0x0806E465
 		str r4, [r7,0]
+
+		ldr r7, =0x0203AD20
+		mov r4, 0x0
+		str r4,[r7,0]
+
 
 		ldr r0, =EntityDataP1
 		ldr r5, =0x0806E465
@@ -130,11 +151,11 @@ ChooseAbility:
 		mov lr, r6
 		push {r1-r3}
 		bx r5
+
 		done:
 		pop {r4-r7}
 		pop {r0}
 		bx r0
-
 
 
 
